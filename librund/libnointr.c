ae7fa013d6cd9c66a85fd65b8684bbddabfc5b6a
Nick Clark <nicholas.clark@gmail.com>
2018-04-11 10:12:00 -0600

    Libnointr: add additional library functios

--- librund/libnointr.c
+++ librund/libnointr.c
@@ -1,9 +1,13 @@
+#include "config.h"
+
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/select.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "libnointr.h"
@@ -120,6 +124,68 @@ pid_t waitpid_nointr(pid_t pid, int *stat_loc, int options)
 
     while (1) {
         result = waitpid(pid, stat_loc, options);
+
+        if ((result < 0) && (errno == EINTR)) {
+            continue;
+        }
+
+        return result;
+    }
+}
+
+int accept_nointr(int socket, struct sockaddr *restrict address,
+                  socklen_t *restrict address_len)
+{
+    int result;
+
+    while (1) {
+        result = accept(socket, address, address_len);
+
+        if ((result < 0) && (errno == EINTR)) {
+            continue;
+        }
+
+        return result;
+    }
+}
+
+int connect_nointr(int socket, const struct sockaddr *address,
+                   socklen_t address_len)
+{
+    int result;
+
+    while (1) {
+        result = connect(socket, address, address_len);
+
+        if ((result != 0) && (errno == EINTR)) {
+            continue;
+        }
+
+        return result;
+    }
+}
+
+int dup2_nointr(int fildes, int fildes2)
+{
+    int result;
+
+    while (1) {
+        result = dup2(fildes, fildes2);
+
+        if ((result < 0) && (errno == EINTR)) {
+            continue;
+        }
+
+        return result;
+    }
+}
+
+int nanosleep_nointr(const struct timespec *rqtp, struct timespec *rmtp)
+{
+    int result;
+
+    while (1) {
+        result = nanosleep(rqtp, rmtp);
 
         if ((result < 0) && (errno == EINTR)) {
             continue;
--- librund/libnointr.h
+++ librund/libnointr.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 #include <sys/select.h>
+#include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 int select_nointr(int nfds, fd_set *restrict readfds, fd_set *restrict writefds,
@@ -20,5 +22,15 @@ FILE * fopen_nointr(const char *restrict pathname, const char *restrict mode);
 int fclose_nointr(FILE *stream);
 
 pid_t waitpid_nointr(pid_t pid, int *stat_loc, int options);
+
+int accept_nointr(int socket, struct sockaddr *restrict address,
+                  socklen_t *restrict address_len);
+
+int connect_nointr(int socket, const struct sockaddr *address,
+                   socklen_t address_len);
+
+int dup2_nointr(int fildes, int fildes2);
+
+int nanosleep_nointr(const struct timespec *rqtp, struct timespec *rmtp);
 
 #endif
